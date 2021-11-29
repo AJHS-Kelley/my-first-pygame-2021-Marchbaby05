a98c92ce7f3148fc5753289f65d91b6455c5e253
Areillee B <s11349941@students.duvalschools.org>
2021-11-29 14:17:21 -0500

    v0.1

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,10 @@
-# My First PyGame, Areillee Butler, 11/29/21 1:58pm, v0.0
+# My First PyGame, Areillee Butler, 11/29/21 1:58pm, v0.1
 
 import pygame, sys 
 from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+# Setup our window. 1 
+windowSurface = pygame.display.set_mode((500, 400), 0, 32) 
