b14def42064719b4d2e25d36434c8b12d45897fa
Areillee B <s11349941@students.duvalschools.org>
2021-12-01 13:55:50 -0500

    v0.6

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Areillee Butler, 11/29/21 2:50pm, v0.5
+# My First PyGame, Areillee Butler, 12/1/21 1:55pm, v0.6
 
 import pygame, sys 
 from pygame.locals import *
@@ -16,7 +16,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+FLOWERPINK = (255, 204, 255)
 # Setup font. 
 basicFont = pygame.font.SysFont(None, 48)
 
@@ -26,8 +26,14 @@ textReact = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx 
 textRect.centery = windowSurface.get_rect().centery
 
-#  Fill background color. 
-windowSurface.fill(WHITE)
 
-# Draw a polygon onto the screen. 
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277)(56, 277), (0,106)))
+#  Fill in the background 
+windowSurface.fill(FLOWERPINK)
+
+#  Draw a polygon onto the screen. 
+pygame.draw.polygon(windowSurface, FLOWERPINK, ((146, 0), (291, 106), (236, 277)(56, 277), (0,106))) 
+
+# Draw lines on the screen. 
+pygame.draw.line(windowSurface,  BLUE,  (60,60,) (120, 60), 4) 
+pygame.draw.line(windowSurface,  RED,  (0,150,) (60, 75), 1) 
+pygame.draw.line(windowSurface,  WHITE,  (75,60,) (60, 75), 2) 
