006a73518a523487d4f3497675cda2c2afa24daf
Areillee B <s11349941@students.duvalschools.org>
2021-12-09 14:21:23 -0500

    v0.3
    Added color values for white, red, green, and blue.

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Areillee Butler, 2:16, 12/09/21, v0.2
+# Simple Animation with PyGame, Areillee Butler, 2:20, 12/09/21, v0.3
 
 import pygame, sys, time 
 from pygame.locals import *
@@ -19,3 +19,9 @@ UPLEFT = 'upleft'
 UPRIGHT = 'upright'
 
 MOVESPEED = 4
+
+# Setup color values. 
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
